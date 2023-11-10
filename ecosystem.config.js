6fcfcef18a73b8ecbc14365ccc98e58994131570
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-11-10 15:55:25 +0000

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
       {
         name: 'master',
         script: './master.js',
+        exec_mode: 'fork',
         instances: 1,
         autorestart: true,
         watch: false,
@@ -10,16 +11,22 @@ module.exports = {
       {
         name: 'bmap_listener',
         script: './bmap_listener.js',
+        exec_mode: 'fork',
         instances: 1,
+        wait_ready: true,
         autorestart: true,
         watch: false,
+        dependencies: ['master']
       },
       {
         name: 'lockup_listener',
         script: './lockup_listener.js',
+        exec_mode: 'fork',
         instances: 1,
+        wait_ready: true,
         autorestart: true,
         watch: false,
+        dependencies: ['master']
       },
     ],
 };
