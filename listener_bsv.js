09eaaeb02bd6c17072ab73dd70f8eda73c1fd232
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-12-04 17:15:20 +0100

    updates

--- listener_bsv.js
+++ listener_bsv.js
@@ -44,7 +44,7 @@ const onBlock = ({
                 size,
                 height,
                 txCount,
-                txs,
+                //txs,
                 startDate,
             })
     ))
